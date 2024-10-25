1e61f1bf8417731e1b6ac386497fa96ac8538d2f
uncaminante <uncaminante.es@gmail.com>
2024-10-25 17:32:05 +0200

    Reloj listo

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 let watch = document.getElementById("watch");
-watch.innerHTML = "<h2>Here is the watch</h2>"
+watch.innerHTML = "<h2>Here is the watch</h2>";
+
+
+let theWatch = setInterval(()=>{
+    let timenow = new Date();
+    let hour = timenow.getHours();
+    let mins = timenow.getMinutes();
+    let secs = timenow.getSeconds();
+    watch.innerHTML = `<h1>${hour} : ${mins} : ${secs}</h1>`;
+    
+},1000)
